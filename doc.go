865307988babedae8ea28c472691ec2fe448577c
Will Roden <willabides@github.com>
2020-02-18 10:22:17 -0600

    replace _examples with test examples.

--- doc.go
+++ doc.go
@@ -1,5 +1,9 @@
+//nolint:golint
+
 /*
-Package kongplete lets you generate shell completions for your command-line programs using
+You kongplete me.
+
+kongplete lets you generate shell completions for your command-line programs using
 github.com/alecthomas/kong and github.com/posener/complete.
 */
 package kongplete
--- example_test.go
+++ example_test.go
@@ -1,10 +1,55 @@
-package kongplete
+// This example is adapted from the shell example in github.com/alecthomas/kong
 
-func ExampleComplete() {
-	var cli struct {
-		Foo struct {
-		} `kong:"cmd"`
+package kongplete_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/posener/complete"
+	"github.com/willabides/kongplete"
+)
+
+var shellCli struct {
+	Rm struct {
+		User      string `help:"Run as user." short:"u" default:"default"`
+		Force     bool   `help:"Force removal." short:"f"`
+		Recursive bool   `help:"Recursively remove files." short:"r"`
+
+		Paths []string `arg:"" help:"Paths to remove." type:"path" name:"path" predictor:"file"`
+	} `cmd:"" help:"Remove files."`
+
+	Ls struct {
+		Paths []string `arg:"" optional:"" help:"Paths to list." type:"path" predictor:"file"`
+	} `cmd:"" help:"List paths."`
+
+	Debug bool `help:"Debug mode."`
+
+	InstallCompletions kongplete.InstallCompletions `cmd:"" help:"install shell completions"`
+}
+
+func Example() {
+	// Create a kong parser as usual, but don't run Parse quite yet.
+	parser := kong.Must(&shellCli,
+		kong.Name("shell"),
+		kong.Description("A shell-like example app."),
+		kong.UsageOnError(),
+	)
+
+	// Run kongplete.Complete to handle completion requests
+	kongplete.Complete(parser,
+		kongplete.WithPredictor("file", complete.PredictFiles("*")),
+	)
+
+	// Proceed as normal after kongplete.Complete.
+	ctx, err := parser.Parse(os.Args[1:])
+	parser.FatalIfErrorf(err)
+
+	switch ctx.Command() {
+	case "rm <path>":
+		fmt.Println(shellCli.Rm.Paths, shellCli.Rm.Force, shellCli.Rm.Recursive)
+
+	case "ls":
 	}
-
-	_ = cli
 }
